5918de7b0972719175b942065adcd98f0ceba709
Nikhil_Chavan <nikhilchavan1272@gmail.com>
2026-04-11 02:49:25 +0530

    Add database models

---
+++ backend/models/logModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const logSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  action: {
+    type: String,
+    required: true
+  },
+  ip: {
+    type: String
+  },
+  details: {
+    type: mongoose.Schema.Types.Mixed
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Log', logSchema);
